f84d71b5aac76b76680faa22b032136eb28d90e5
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2021-10-11 17:04:44 -0300

    check if godot is in cache

--- dist/index.js
+++ dist/index.js
@@ -41,28 +41,36 @@ function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const godotVersion = core.getInput('godot-version');
-            let godot_exec = '';
-            if (process.platform === 'win32') {
-                core.info(`Downloading Godot ${godotVersion} for windows`);
-                godot_exec = 'win32.exe';
-            }
-            else if (process.platform === 'linux') {
-                core.info(`Downloading Godot ${godotVersion} headless for linux`);
-                godot_exec = 'linux_headless.64';
+            let godotPath = tc.find('godot', godotVersion, process.platform);
+            if (godotPath) {
+                core.info(`Godot ${godotVersion} found in cache! Path: ${godotPath}`);
             }
             else {
-                throw Error(`Unsuported OS: ${process.platform}`);
+                let godot_exec = '';
+                if (process.platform === 'win32') {
+                    core.info(`Attempting to download Godot ${godotVersion} for windows...`);
+                    godot_exec = 'win32.exe';
+                }
+                else if (process.platform === 'linux') {
+                    core.info(`Attempting to download Godot ${godotVersion} headless for linux...`);
+                    godot_exec = 'linux_headless.64';
+                }
+                else {
+                    throw Error(`Unsuported OS: ${process.platform}`);
+                }
+                const godotFileName = `Godot_v${godotVersion}-stable_${godot_exec}`;
+                const godotDownloadPath = yield tc.downloadTool(`https://downloads.tuxfamily.org/godotengine/${godotVersion}/${godotFileName}.zip`);
+                core.info(`Godot ${godotVersion} donwload sucessfull!`);
+                core.info(`Attempting to extracting Godot ${godotVersion}`);
+                const godotExtractPath = yield tc.extractZip(godotDownloadPath, undefined);
+                core.info(`Godot ${godotVersion} extracted to ${godotExtractPath}`);
+                core.info('Adding to cache...');
+                godotPath = yield tc.cacheFile(`${godotExtractPath}/${godotFileName}`, 'godot', 'godot', godotVersion, process.platform);
+                core.info(`Godot ${godotVersion} cached!`);
             }
-            const godotFileName = `Godot_v${godotVersion}-stable_${godot_exec}`;
-            const godotPath = yield tc.downloadTool(`https://downloads.tuxfamily.org/godotengine/${godotVersion}/${godotFileName}.zip`);
-            core.info(`Godot ${godotVersion} donwloaded sucessfull!`);
-            core.info(`Extracting Godot ${godotVersion}`);
-            const godotExtractPath = yield tc.extractZip(godotPath, undefined);
-            core.info(`Godot ${godotVersion} extracted to ${godotExtractPath}`);
-            core.info('Adding to cache and path');
-            const godotCachedPath = yield tc.cacheFile(`${godotExtractPath}/${godotFileName}`, 'godot', 'godot', godotVersion, process.platform);
-            core.addPath(godotCachedPath);
-            core.info('Done!');
+            core.info('Adding to path...');
+            core.addPath(godotPath);
+            core.info(`Godot ${godotVersion} added to path!`);
             core.info(`Godot ${godotVersion} is ready to use!`);
             core.setOutput('time', new Date().toTimeString());
         }
--- src/main.ts
+++ src/main.ts
@@ -5,39 +5,40 @@ async function run(): Promise<void> {
   try {
     const godotVersion: string = core.getInput('godot-version');
 
-    let godot_exec = '';
+    let godotPath = tc.find('godot', godotVersion, process.platform);
 
-    if (process.platform === 'win32') {
-      core.info(`Downloading Godot ${godotVersion} for windows`);
-      godot_exec = 'win32.exe';
-    } else if (process.platform === 'linux') {
-      core.info(`Downloading Godot ${godotVersion} headless for linux`);
-      godot_exec = 'linux_headless.64';
+    if (godotPath) {
+      core.info(`Godot ${godotVersion} found in cache! Path: ${godotPath}`);
     } else {
-      throw Error(`Unsuported OS: ${process.platform}`);
+      let godot_exec = '';
+
+      if (process.platform === 'win32') {
+        core.info(`Attempting to download Godot ${godotVersion} for windows...`);
+        godot_exec = 'win32.exe';
+      } else if (process.platform === 'linux') {
+        core.info(`Attempting to download Godot ${godotVersion} headless for linux...`);
+        godot_exec = 'linux_headless.64';
+      } else {
+        throw Error(`Unsuported OS: ${process.platform}`);
+      }
+
+      const godotFileName = `Godot_v${godotVersion}-stable_${godot_exec}`;
+
+      const godotDownloadPath = await tc.downloadTool(`https://downloads.tuxfamily.org/godotengine/${godotVersion}/${godotFileName}.zip`);
+      core.info(`Godot ${godotVersion} donwload sucessfull!`);
+
+      core.info(`Attempting to extracting Godot ${godotVersion}`);
+      const godotExtractPath = await tc.extractZip(godotDownloadPath, undefined);
+      core.info(`Godot ${godotVersion} extracted to ${godotExtractPath}`);
+
+      core.info('Adding to cache...');
+      godotPath = await tc.cacheFile(`${godotExtractPath}/${godotFileName}`, 'godot', 'godot', godotVersion, process.platform);
+      core.info(`Godot ${godotVersion} cached!`);
     }
 
-    const godotFileName = `Godot_v${godotVersion}-stable_${godot_exec}`;
-
-    const godotPath = await tc.downloadTool(
-      `https://downloads.tuxfamily.org/godotengine/${godotVersion}/${godotFileName}.zip`
-    );
-    core.info(`Godot ${godotVersion} donwloaded sucessfull!`);
-
-    core.info(`Extracting Godot ${godotVersion}`);
-    const godotExtractPath = await tc.extractZip(godotPath, undefined);
-    core.info(`Godot ${godotVersion} extracted to ${godotExtractPath}`);
-
-    core.info('Adding to cache and path');
-    const godotCachedPath = await tc.cacheFile(
-      `${godotExtractPath}/${godotFileName}`,
-      'godot',
-      'godot',
-      godotVersion,
-      process.platform
-    );
-    core.addPath(godotCachedPath);
-    core.info('Done!');
+    core.info('Adding to path...');
+    core.addPath(godotPath);
+    core.info(`Godot ${godotVersion} added to path!`);
 
     core.info(`Godot ${godotVersion} is ready to use!`);
 
