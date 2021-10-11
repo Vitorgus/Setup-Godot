2b3dea6832603fc89a98fe3b1b5204010bb16cde
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2021-10-11 14:06:57 -0300

    attempt do download godot headless

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
 import * as core from '@actions/core';
-import {wait} from './wait';
+import * as tc from '@actions/tool-cache';
+// import {wait} from './wait';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds');
-    core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    // const ms: string = core.getInput('milliseconds');
 
-    core.debug(new Date().toTimeString());
-    await wait(parseInt(ms, 10));
-    core.debug(new Date().toTimeString());
+    const godotPath = await tc.downloadTool(
+      'https://downloads.tuxfamily.org/godotengine/3.3.4/Godot_v3.3.4-stable_linux_headless.64.zip'
+    );
+    core.info(`Godot donwloaded to ${godotPath}`);
 
-    core.info('This is an info message.');
-    console.log('This is a console.log message');
+    // core.info('This is an info message.');
+    // console.log('This is a console.log message');
+
+    core.info(`The plataform is ${process.platform}`);
 
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
