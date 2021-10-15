df5e3659aca96d784ba16d41f521d4dd1dd691ba
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2021-10-15 20:59:42 -0300

    first test written

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,15 +1,39 @@
-// import * as process from "process";
-import * as cp from "child_process";
-import * as path from "path";
-import { expect, test } from "@jest/globals";
+import * as core from "@actions/core";
+import * as tc from "@actions/tool-cache";
+import * as io from "@actions/io";
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test("test runs", () => {
-  process.env["INPUT_MILLISECONDS"] = "500";
-  const np = process.execPath;
-  const ip = path.join(__dirname, "..", "lib", "main.js");
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  };
-  // console.log(cp.execFileSync(np, [ip], options).toString());
+import { getGodot } from "../src/get-tools";
+
+jest.mock("@actions/core");
+jest.mock("@actions/tool-cache");
+jest.mock("@actions/io");
+
+interface InputsInterface {
+  [name: string]: string;
+}
+
+type MockedCoreGetInput = jest.MockedFunction<typeof core.getInput>;
+type MockedTcFind = jest.MockedFunction<typeof tc.find>;
+
+let inputs: InputsInterface;
+
+let mockCoreGetInput: MockedCoreGetInput;
+let mockTcFind: MockedTcFind;
+
+beforeEach(() => {
+  // process.env['GITHUB_PATH'] = ''; // Stub out ENV file functionality so we can verify it writes to standard out
+  inputs = {};
+
+  mockCoreGetInput = core.getInput as MockedCoreGetInput;
+  mockCoreGetInput.mockImplementation((name: string) => inputs[name]);
+
+  mockTcFind = tc.find as MockedTcFind;
+});
+
+describe("getGodot tests", () => {
+  test("Cache test", async () => {
+    mockTcFind.mockReturnValue("path/to/cache");
+    await getGodot("3.3.2", false);
+    expect(core.info).toBeCalledWith("Godot 3.3.2 found in cache! Path: path/to/cache");
+  });
 });
